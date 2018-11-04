b2e8955d0e5e3e5a8fc2709e2d89556475434648
Lindsay Pink <43824268+LindsayPink@users.noreply.github.com>
2018-11-04 00:46:16 -0500

    made allPiece and pieces global, corrected tilePlace to use grid properties

--- fifteen.js
+++ fifteen.js
@@ -1,6 +1,6 @@
 window.onload = function () {
-    
-    let allPiece = document.body.children[2].children[0].children;
+    allPiece = document.body.children[2].children[0].children;
+    pieces = Array.from(allPiece);
     $(allPiece).addClass("puzzlepiece");
 
     bckgrndPos();
@@ -8,7 +8,6 @@ window.onload = function () {
 }
 
 function bckgrndPos () {
-    let allPiece = document.body.children[2].children[0].children;
     $(allPiece[0]).css("background-position",  "0000px 0000px",);
     $(allPiece[1]).css("background-position",  "-100px 0000px");
     $(allPiece[2]).css("background-position",  "-200px 0000px");
@@ -27,9 +26,23 @@ function bckgrndPos () {
 }
 
 function tilePlace () {
-    let allPiece = document.body.children[2].children[0].children;
     $("#puzzlearea").css("display", "grid");
     $("#puzzlearea").css("grid-template-columns", "100px 100px 100px 100px");
     $("#puzzlearea").css("grid-template-rows", "100px 100px 100px 100px");
-    $(allPiece).css("position", "relative");
+    let row1 = pieces.slice(0,4);
+    let row2 = pieces.slice(4,8);
+    let row3 = pieces.slice(8,12);
+    let row4 = pieces.slice(12,15);
+    for (let i = 0, r = 1; i <= row1.length; i++) {
+        $(row1[i]).css({"grid-row-start":r.toString(), "grid-column-start":(i+1).toString()});
+    }
+    for (let i = 0, r = 2; i <= row2.length; i++) {
+        $(row2[i]).css({"grid-row-start":r.toString(), "grid-column-start":(i+1).toString()});
+    }
+    for (let i = 0, r = 3; i <= row3.length; i++) {
+        $(row3[i]).css({"grid-row-start":r.toString(), "grid-column-start":(i+1).toString()});
+    }
+    for (let i = 0, r = 4; i <= row4.length; i++) {
+        $(row4[i]).css({"grid-row-start":r.toString(), "grid-column-start":(i+1).toString()});
+    }
 }
