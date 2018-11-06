6a1a1a23433a808f64967f15ceabf7386586a27f
Lindsay Pink <43824268+LindsayPink@users.noreply.github.com>
2018-11-06 13:27:30 -0500

    removed unused tile location id's

--- fifteen.js
+++ fifteen.js
@@ -40,19 +40,15 @@ function tilePlace () {
     let row4 = pieces.slice(12,15);
     for (let i = 0, r = 1; i <= row1.length; i++) {
         $(row1[i]).css({"grid-row-start":r.toString(), "grid-column-start":(i+1).toString()});
-        $(row1[i]).attr("id", "tile_"+((r).toString())+"_"+((i+1).toString()));
     }
     for (let i = 0, r = 2; i <= row2.length; i++) {
         $(row2[i]).css({"grid-row-start":r.toString(), "grid-column-start":(i+1).toString()});
-        $(row2[i]).attr("id", "tile_"+((r).toString())+"_"+((i+1).toString()));
     }
     for (let i = 0, r = 3; i <= row3.length; i++) {
         $(row3[i]).css({"grid-row-start":r.toString(), "grid-column-start":(i+1).toString()});
-        $(row3[i]).attr("id", "tile_"+((r).toString())+"_"+((i+1).toString()));
     }
     for (let i = 0, r = 4; i <= row4.length; i++) {
         $(row4[i]).css({"grid-row-start":r.toString(), "grid-column-start":(i+1).toString()});
-        $(row4[i]).attr("id", "tile_"+((r).toString())+"_"+((i+1).toString()));
     }
 }
 
@@ -60,7 +56,6 @@ function findTile (r, c) {
     for (let i = 0; i <= pieces.length; i++) {
         if ((parseInt($(pieces[i]).css("grid-row-start")))===r && 
         ((parseInt($(pieces[i]).css("grid-column-start"))))===c) {
-            $(pieces[i]).attr("id", "tile_"+((r).toString())+"_"+((c).toString()));
             return pieces[i];
         }
     }
@@ -90,7 +85,6 @@ function empty () {
 function move (tile) {
     if (tile.classList[1] === "movablepiece") { 
         $(tile).css({"grid-row-start":free_row.toString(), "grid-column-start":free_col.toString()});
-        $(tile).attr("id", "tile_"+((free_row).toString())+"_"+((free_col).toString()));
         empty();
         moveable();
         try_move();
